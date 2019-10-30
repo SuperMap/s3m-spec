37369c382ac087e188f211051c8fb4da472a7528
heqianhq <heqian@supermap.com>
2019-10-30 18:10:27 +0800

    添加文件

---
+++ S3M_SDK/S3M_Parser_JS/js/S3MTiles/Enum/VertexCompressOption.js
@@ -0,0 +1,15 @@
+define([
+    ], function(
+        ) {
+    'use strict';
+
+    let VertexCompressOptions = {
+        SVC_Vertex : 1,
+        SVC_Normal : 2,
+        SVC_VertexColor : 4,
+        SVC_SecondColor	: 8,
+        SVC_TexutreCoord : 16,
+        SVC_TexutreCoordIsW	: 32
+    };
+    return Object.freeze(VertexCompressOptions);
+});
