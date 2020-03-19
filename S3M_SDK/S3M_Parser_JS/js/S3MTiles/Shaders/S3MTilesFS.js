3d4bd87ae4e6646f03fcb248c76fc8f40f216a67
heqianhq <heqian@supermap.com>
2020-03-19 11:42:40 +0800

    修改范例代码

--- S3M_SDK/S3M_Parser_JS/js/S3MTiles/Shaders/S3MTilesFS.js
+++ S3M_SDK/S3M_Parser_JS/js/S3MTiles/Shaders/S3MTilesFS.js
@@ -1,82 +1,19 @@
 define(function() {
     'use strict';
     return `
-    #ifdef GL_OES_standard_derivatives
-        #extension GL_OES_standard_derivatives : enable
-    #endif
-    #ifdef TexCoord
         uniform sampler2D uTexture;
-        uniform float uTextureWidth;
-        varying vec4 vTexCoord;
-        varying vec4 vTexMatrix;
-    #endif
     #ifdef TexCoord2
         uniform sampler2D uTexture2;
-        uniform float uTexture2Width;
-        varying vec4 vTexMatrix2;
-    #endif
-    #ifdef VertexNormal
-        varying vec3 vNormalEC;
     #endif
         uniform vec4 uDiffuseColor;
         uniform vec4 uAmbientColor;
         varying vec4 vColor;
-    #ifdef TexCoord
-        void calculateTexCoord(in vec2 inTexCoord, in float scale, in float XTran, in float YTran, in float fTile, in float mipLevel, inout vec2 outTexCoord)
-        {
-            vec2 fTexCoord = fract(inTexCoord);
-            float offset = 1.0 * pow(2.0, mipLevel) / fTile;
-            fTexCoord = clamp(fTexCoord, offset, 1.0 - offset);
-            outTexCoord.x = (fTexCoord.x + XTran) * scale;
-            outTexCoord.y = (fTexCoord.y + YTran) * scale;
-        }
-        vec4 sampleTexture(sampler2D curTexture, vec2 oriTexCoord, float texTileWidth, float fMaxMipLev, float fTexCoordScale, vec2 vecTexCoordTranslate)
-        {
-            vec2 realTexCoord;
-            calculateTexCoord(oriTexCoord, fTexCoordScale, vecTexCoordTranslate.x, vecTexCoordTranslate.y, texTileWidth, 0.0, realTexCoord);
-            vec4 color = texture2D(curTexture, realTexCoord.xy);
-            return color;
-        }
-    #endif
-    #ifdef TexCoord
-        vec4 getTextureColor()
-        {
-            if(vTexMatrix.z < 0.0)
-            {
-                return vec4(1.0);
-            }
-            float texTileWidth = vTexMatrix.z * uTextureWidth;
-            vec4 color = sampleTexture(uTexture, vTexCoord.xy, texTileWidth, vTexMatrix.w, vTexMatrix.z, vTexMatrix.xy);
-            return color;
-        }
-    #endif
-    #ifdef TexCoord2
-        vec4 getTexture2Color()
-        {
-            if(vTexMatrix2.z < 0.0)
-            {
-                return vec4(1.0);
-            }
-            float texTileWidth = vTexMatrix2.z * uTexture2Width;
-            vec4 color = sampleTexture(uTexture2, vTexCoord.zw, texTileWidth, vTexMatrix2.w, vTexMatrix2.z, vTexMatrix2.xy);
-            color.r = clamp(color.r * 1.618, 0.0, 1.0);
-            color.g = clamp(color.g * 1.618, 0.0, 1.0);
-            color.b = clamp(color.b * 1.618, 0.0, 1.0);
-            return color;
-        }
-    #endif
+        varying vec4 vTexCoord;
         void main()
         {
-            vec4 color = vColor;
-        #ifdef TexCoord
-            color *= getTextureColor();
-        #endif
+            vec4 color = vColor * texture2D(uTexture, vTexCoord.xy);
         #ifdef TexCoord2
-            color *= getTexture2Color();
-        #endif
-        #ifdef VertexNormal
-            float diffuseIntensity = max(dot(czm_sunDirectionEC, normalize(vNormalEC)), 0.0);
-            color = vec4(color.rgb * (vec3(0.8) * uDiffuseColor.rgb * diffuseIntensity + vec3(0.6) * uAmbientColor.rgb), color.a);
+            color *= texture2D(uTexture2, vTexCoord.zw);
         #endif
             gl_FragColor = czm_gammaCorrect(color);
         }
