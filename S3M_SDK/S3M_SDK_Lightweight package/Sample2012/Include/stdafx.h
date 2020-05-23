9027de8d94a97af4642b60abca28d81b7fbe10d3
heqianhq <heqian@supermap.com>
2020-05-23 12:56:10 +0800

    删除旧包

--- S3M_SDK/S3M_SDK_Lightweight package/Sample2012/Include/stdafx.h
+++
@@ -1,5 +0,0 @@
-using namespace std;
-#define unsigned int uInt;
-#define unsigned short uShort;
-#define unsigned long uLong;
-#define unsigned char uChar;
--- S3M_SDK/S3M_SDK_Lightweight package/Sample2019/Include/stdafx.h
+++
@@ -1,5 +0,0 @@
-using namespace std;
-#define unsigned int unsigned int;
-#define unsigned short unsigned short;
-#define unsigned long uLong;
-#define unsigned char uChar;
